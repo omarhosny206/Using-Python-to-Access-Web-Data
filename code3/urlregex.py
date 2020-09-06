695a329025f8c2583e6d1e430b9d9652e1904d12
omarhosny206 <omarhosny206>
2020-09-06 21:55:06 +0200

    Using Python to Access Web Data On Coursera

---
+++ code3/urlregex.py
@@ -0,0 +1,15 @@
+# Search for link values within URL input
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+links = re.findall(b'href="(http[s]?://.*?)"', html)
+for link in links:
+    print(link.decode())
